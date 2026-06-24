305a694689d0af1596912fd796749985243fd40d
JoaoLopez <joao.p.lopez01@gmail.com>
2026-06-24 05:54:56 +0000

    Updating inputs

--- menger_sponge_speedupy_4100.py
+++ menger_sponge_speedupy_4100.py
@@ -7,7 +7,7 @@ from numpy import empty, array
 from matplotlib.pylab import imshow, cm, show
 
 @deterministic
-def outside_unit_cube(triple):
+def outside_unit_cube(triple, func_globals=None):
     (x, y, z) = triple
     if x < 0 or y < 0 or z < 0:
         return 1
@@ -16,11 +16,11 @@ def outside_unit_cube(triple):
     return 0
 
 @deterministic
-def in_sponge(triple, level):
+def in_sponge(triple, level, func_globals=None):
     """Determine whether a point lies inside the Menger sponge
     after the number of iterations given by 'level.' """
     (x, y, z) = triple
-    if outside_unit_cube(triple):
+    if outside_unit_cube(triple, func_globals=globals()):
         return 0
     if x == 1 or y == 1 or z == 1:
         return 1
@@ -40,13 +40,13 @@ def in_sponge(triple, level):
     return 1
 
 @deterministic
-def cross_product(v, w):
+def cross_product(v, w, func_globals=None):
     (v1, v2, v3) = v
     (w1, w2, w3) = w
     return (v2 * w3 - v3 * w2, v3 * w1 - v1 * w3, v1 * w2 - v2 * w1)
 
 @deterministic
-def length(v):
+def length(v, func_globals=None):
     """Euclidean length"""
     (x, y, z) = v
     return sqrt(x * x + y * y + z * z)
@@ -64,17 +64,17 @@ def plot_slice(normal, point, level, n):
         t = (1, 0, 1)
     else:
         t = (1, 1, 0)
-    cross = cross_product(normal, t)
-    v = array(cross) / length(cross)
-    cross = cross_product(normal, v)
-    w = array(cross) / length(cross)
+    cross = cross_product(normal, t, func_globals=globals())
+    v = array(cross) / length(cross, func_globals=globals())
+    cross = cross_product(normal, v, func_globals=globals())
+    w = array(cross) / length(cross, func_globals=globals())
     m = empty((n, n), dtype=int)
     h = 1.0 / (n - 1)
     k = 2.0 * sqrt(3.0)
     for x in range(n):
         for y in range(n):
             pt = point + (h * x - 0.5) * k * v + (h * y - 0.5) * k * w
-            m[x, y] = 1 - in_sponge(pt, level)
+            m[x, y] = 1 - in_sponge(pt, level, func_globals=globals())
     imshow(m, cmap=cm.gray)
     show(block=False)
 
@@ -83,6 +83,6 @@ def main():
     normal = (1, 1, 0.5)
     point = (0.5, 0.5, 0.5)
     level = 3
-    n = 4000
+    n = 4100
     plot_slice(normal, point, level, n)
 main()
